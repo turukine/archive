2f2210a32441905a428ff8eb5cee22e70138a6fd
turukine <turukin_egor@mail.ru>
2025-04-14 20:03:28 +0300

    Delete unzip_file.py

--- unzip_file.py
+++
@@ -1,9 +0,0 @@
-import zipfile
-import os
-def unzip_file(zip_path, extract_to="."):
-    with zipfile.ZipFile(zip_path, 'r') as zipf:
-        zipf.extractall(extract_to)
-    
-    print(f"Файлы из {zip_path} распакованы в {extract_to}")
-
-unzip_file("archive.zip", "extracted_files")
